a303154ef0d86d027bf69ac1c06e6bd4aeb343c7
Franco Colmenarez <francoacg1@gmail.com>
2021-02-05 13:15:12 -0500

    Theming

--- lib/main.dart
+++ lib/main.dart
@@ -12,8 +12,10 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.purple,
+        accentColor: Colors.deepOrange,
         visualDensity: VisualDensity.adaptivePlatformDensity,
+        fontFamily: 'Lato',
       ),
       home: ProductsOverviewScreen(),
     );
