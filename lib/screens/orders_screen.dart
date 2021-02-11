3a3496f1a05227bb262afb4600b2f220ba26064e
Franco Colmenarez <francoacg1@gmail.com>
2021-02-11 11:09:41 -0500

    Using FutureProvider

--- lib/screens/orders_screen.dart
+++ lib/screens/orders_screen.dart
@@ -5,46 +5,33 @@ import '../providers/orders.dart';
 import '../widgets/order_list_item.dart';
 import '../widgets/app_drawer.dart';
 
-class OrdersScreen extends StatefulWidget {
+class OrdersScreen extends StatelessWidget {
   static const routeName = '/orders';
 
   @override
-  _OrdersScreenState createState() => _OrdersScreenState();
-}
-
-class _OrdersScreenState extends State<OrdersScreen> {
-  bool _isInit = true;
-  bool _isLoading = false;
-
-  @override
-  void didChangeDependencies() {
-    super.didChangeDependencies();
-    if (_isInit) {
-      setState(() {
-        _isLoading = true;
-        print(_isLoading);
-      });
-      Provider.of<Orders>(context).fetchAndSetOrders().then((_) {
-        setState(() {
-          _isLoading = false;
-          print(_isLoading);
-        });
-      });
-    }
-    _isInit = false;
-  }
-
-  @override
   Widget build(BuildContext context) {
-    final orderData = Provider.of<Orders>(context);
-
     return Scaffold(
       appBar: AppBar(
         title: Text('Your orders'),
       ),
-      body: _isLoading ? Center(child: CircularProgressIndicator(),) : ListView.builder(
-        itemCount: orderData.orders.length,
-        itemBuilder: (context, index) => OrderListItem(orderData.orders[index]),
+      body: FutureBuilder(
+        future: Provider.of<Orders>(context, listen: false).fetchAndSetOrders(),
+        builder: (context, dataSnapshot) {
+          if (dataSnapshot.connectionState == ConnectionState.waiting) {
+            return Center(child: CircularProgressIndicator(),);
+          }
+          if (dataSnapshot.error != null) {
+            return Center(
+              child: Text('An error ocurred!'),
+            );
+          }
+          return Consumer<Orders>(
+            builder: (context, orderData, child) => ListView.builder(
+              itemCount: orderData.orders.length,
+              itemBuilder: (context, index) => OrderListItem(orderData.orders[index]),
+            ),
+          );
+        },
       ),
       drawer: AppDrawer(),
     );
